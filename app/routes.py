a5f8747a265cdd3697cbac259f5f7a802970841b
FlexiEndpoints <81296586+s34azuread@users.noreply.github.com>
2024-08-22 16:04:07 +0530

    Making source port configurable

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,7 @@ router = APIRouter()
 @router.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+@router.get("/items_2/{item_id}")
+def read_item(item_id: int, q: str = None, r: str = None):
+    return {"item_id": item_id, "q": q, "r" : r}
